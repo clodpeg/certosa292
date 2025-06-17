ac348fca1b6ddd2461046be2890d75c7d8981ba7
clodpeg <certosa292@gmail.com>
2025-06-17 10:30:32 +0200

    x

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,15 @@ const Certosa292 = {
     modalWifi: null,
     modalRules: null,
     modalGuide: null,
+    modalDove: null,
+    modalComeMuoversi: null,
+    modalFood: null,
+    modalEmergency: null,
 
     init: function(){
         this.Modals();
         this.Menu();
+        this.Hash();
     },
 
     Modals: () => {
@@ -19,6 +24,27 @@ const Certosa292 = {
         Certosa292.modalWifi = new bootstrap.Modal('#modal-wifi');
         Certosa292.modalRules = new bootstrap.Modal('#modal-rules');
         Certosa292.modalGuide = new bootstrap.Modal('#modal-guide');
+        Certosa292.modalDove = new bootstrap.Modal('#modal-dove-lo-trovo');
+        Certosa292.modalComeMuoversi = new bootstrap.Modal('#modal-come-muoversi');
+        Certosa292.modalFood = new bootstrap.Modal('#modal-food');
+        Certosa292.modalEmergency = new bootstrap.Modal('#modal-emergency');
+
+
+        
+        const modals = document.querySelectorAll('.modal');
+
+        modals.forEach(el => {
+
+            el.addEventListener('hide.bs.modal', () => {
+                if (document.activeElement && el.contains(document.activeElement)) {
+                  document.activeElement.blur();
+                }
+            });
+
+            el.addEventListener('hidden.bs.modal', event => {
+                history.replaceState(null, '', window.location.pathname + window.location.search);
+            })
+        });
 
     },
 
@@ -47,8 +73,39 @@ const Certosa292 = {
                     Certosa292.modalGuide.show();
                 break;
 
+                case 'menu-dove-lo-trovo':
+                    Certosa292.modalDove.show();
+                break;
+
+                case 'menu-come-muoversi':
+                    Certosa292.modalComeMuoversi.show();
+                break;
+
+                case 'menu-food':
+                    Certosa292.modalFood.show();
+                break;
+
+                case 'menu-emergency':
+                    Certosa292.modalEmergency.show();
+                break;
+
             }
+
+            history.replaceState(null, '', window.location.pathname + window.location.search + '#' + $(this).attr('id').replace('menu-', ''));
         });
+    },
+
+    Hash: () => {
+        if ( window.location.hash != ''){
+            let hash = window.location.hash.replace('#','');
+
+            const modalName = 'modal' + hash.charAt(0).toUpperCase() + hash.slice(1);
+                
+            if (Certosa292 && Certosa292[modalName] && typeof Certosa292[modalName].show === 'function') {
+                Certosa292[modalName].show();
+            }
+
+        }
     }
 
 }
